ad654f7a1dc6825295b52ef806aa0352eafd5df6
Vitor Fagundes <vitorfagundes.7@gmail.com>
2025-11-09 14:14:57 -0300

    inclusao do reporter no cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'test/cypress/e2e/**/*.cy.js',
     supportFile: 'test/cypress/support/e2e.js',
-    screenshotsFolder: 'test/cypress/reports/screenshots'
+    screenshotsFolder: 'test/cypress/reports/screenshots',
+    videosFolder: 'test/cypress/reports/videos',
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'test/cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
   }
 });
